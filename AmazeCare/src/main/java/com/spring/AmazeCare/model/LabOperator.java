c9e0689b5d7a3e58c36d51ea0a8b2d29c8f7f558
Anujdev67 <senanuj569@gmail.com>
2024-11-08 01:27:59 +0530

    Initial commit with model classes only

---
+++ AmazeCare/src/main/java/com/spring/AmazeCare/model/LabOperator.java
@@ -0,0 +1,94 @@
+package com.spring.AmazeCare.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class LabOperator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int experience;
+    private double salary;
+    private String contact;
+    private String email;
+    private Date joinedOn;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getJoinedOn() {
+		return joinedOn;
+	}
+
+	public void setJoinedOn(Date joinedOn) {
+		this.joinedOn = joinedOn;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	
+
+    
+}
---
+++ AmazeCare/src/main/java/com/spring/AmazeCare/model/TestAndScans.java
@@ -0,0 +1,30 @@
+package com.spring.AmazeCare.model;
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+@Entity
+public class TestAndScans {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    private String status;
+    private Date askedOn;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    // Getters and Setters
+}
+
